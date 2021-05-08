d2cd62ce2b0a34481d6c81d3796c44d5972d8001
Alex Ynonan <alexynonan@mapsalud.com>
2021-05-08 08:42:09 -0500

    integrate subModule CDA contains general classes

---
+++ ChatBot/ChatBot/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatBot
+//
+//  Created by Alexander on 8/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
